14268892c1968587879c22538e2214e7426e75bb
Suin Kim <contact@suinkim.me>
2025-09-12 04:18:17 +0900

    hotfix: 블로그 썸네일 이미지 명 수정

--- src/config/const.ts
+++ src/config/const.ts
@@ -3,4 +3,4 @@ export const baseDomain = 'https://www.suinkim.me';
 export const blogName = '김민규 개발 블로그';
 export const blogDesc = '개발 지식과 경험을 공유합니다.';
 
-export const blogThumbnailURL = `${baseDomain}/blog-thumbnaill.png`;
+export const blogThumbnailURL = `${baseDomain}/blog-thumbnail.png`;
