81fa854b6d460b69ac81e1178c9403ed38725051
Suin Kim <contact@suinkim.me>
2025-09-14 23:08:06 +0900

    fix: lint

--- src/app/blog/(main)/curation.tsx
+++ src/app/blog/(main)/curation.tsx
@@ -1,28 +1,28 @@
-import { MiniPostCard } from "@/components/post_list/MiniPostCard";
-import PostCard from "@/components/post_list/PostCard";
-import { Post } from "@/config/types";
+import { MiniPostCard } from '@/components/post_list/MiniPostCard';
+import PostCard from '@/components/post_list/PostCard';
+import { Post } from '@/config/types';
 
-export const Curation = ({postList}: {postList: Post[]}) => {
+export const Curation = ({ postList }: { postList: Post[] }) => {
   const firstPost = postList[0];
   const hotPostAllList = postList.filter((post) => post.isHot);
-  const hotPostShowList = firstPost.isHot? hotPostAllList.slice(1,  5) : hotPostAllList.slice(0,  4);
+  const hotPostShowList = firstPost.isHot ? hotPostAllList.slice(1, 5) : hotPostAllList.slice(0, 4);
 
   return (
-    <div className='mx-auto w-full max-w-[1200px] px-4 flex gap-6 lg:gap-8 mt-6 sm:mt-10 flex-col sm:flex-row items-stretch'>
-      <section className="flex-1 w-full flex flex-col min-[980px]:min-w-[520px]">
-        <h2 className='text-lg sm:text-2xl font-bold mb-3'>최신 게시물</h2>
+    <div className='mx-auto mt-6 flex w-full max-w-[1200px] flex-col items-stretch gap-6 px-4 sm:mt-10 sm:flex-row lg:gap-8'>
+      <section className='flex w-full flex-1 flex-col min-[980px]:min-w-[520px]'>
+        <h2 className='mb-3 text-lg font-bold sm:text-2xl'>최신 게시물</h2>
         <div>
-        <PostCard post={firstPost} />
+          <PostCard post={firstPost} />
         </div>
       </section>
-      <section className='flex-1 w-full flex flex-col'>
-        <h2 className='text-lg sm:text-2xl font-bold mb-3'>추천 게시물 🔥</h2>
-        <div className='flex flex-col gap-3 flex-1 '>
+      <section className='flex w-full flex-1 flex-col'>
+        <h2 className='mb-3 text-lg font-bold sm:text-2xl'>추천 게시물 🔥</h2>
+        <div className='grid flex-1 grid-rows-4 gap-3'>
           {hotPostShowList.map((post) => (
             <MiniPostCard key={post.slug} post={post} />
           ))}
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
--- src/app/blog/(main)/page.tsx
+++ src/app/blog/(main)/page.tsx
@@ -1,15 +1,15 @@
+import { Curation } from './curation';
 import PostListPage from '@/components/post_list/PostListPage';
 import { getSortedPostList } from '@/lib/post';
-import { Curation } from './curation';
 
 const Blog = async () => {
   const postList = await getSortedPostList();
-  
+
   return (
     <section>
-      <Curation postList={postList}/>
-      <div className='mx-auto w-full max-w-[1200px] my-10 px-8' >
-      <hr />
+      <Curation postList={postList} />
+      <div className='mx-auto my-10 w-full max-w-[1200px] px-8'>
+        <hr />
       </div>
       <PostListPage postList={postList.slice(1)} />
     </section>
